3dcfd5a62357bffded9f38d3c04e20d794c1d616
nubisub <hendraksm18@mail.ugm.ac.id>
2024-01-22 16:55:49 +0700

    Update footer and sidebar components

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2023 <div class="bullet"></div> SIMWAS Inspektorat Utama
+        &copy; {{ date('Y') }} <div class="bullet"></div> SIMWAS Inspektorat Utama
     </div>
     <div class="footer-right">
         1.0.0
--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -3,145 +3,149 @@
 @section('title', 'Dashboard')
 
 @push('style')
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('library/jqvmap/dist/jqvmap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('library/datatables/media/css/jquery.dataTables.min.css') }}">
+<!-- CSS Libraries -->
+<link rel="stylesheet" href="{{ asset('library/jqvmap/dist/jqvmap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap4.min.css') }}">
+
 @endpush
 
 @section('main')
-    @include('components.header')
-    @include('components.pegawai-sidebar')
-    <div class="main-content">
-        <section class="section">
-            <div class="section-header">
-                <h1>Dashboard</h1>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="fas fa-wrench"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Usulan ST Kinerja</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $stk }}
-                            </div>
-                        </div>
+@include('components.header')
+@include('components.pegawai-sidebar')
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                        <i class="fas fa-wrench"></i>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="fas fa-briefcase"></i>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Usulan ST Kinerja</h4>
                         </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Usulan ST Pengembangan Profesi</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $stp }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-warning">
-                            <i class="fas fa-road"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Usulan ST Perjalanan Dinas</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $stpd }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-success">
-                            <i class="fas fa-file"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Usulan Surat Lain</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $sl }}
-                            </div>
+                        <div class="card-body">
+                            {{ $stk }}
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Surat Saya</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered table-striped display responsive" id="table-pengelolaan-dokumen-pegawai">
-                                        <thead>
-                                            <tr>
-                                                <th>No</th>
-                                                <th>Nomor Surat</th>
-                                                <th>Tanggal Surat</th>
-                                                <th>Jenis Surat</th>
-                                                <th>Download</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($surat as $un)
-                                            <tr>
-                                                <td></td>
-                                                <td>{{ $un->nomor_surat }}</td>
-                                                <td>{{ $un->tanggal }}</td>
-                                                <td>{{ $un->jenis }}</td>
-                                                <td>
-                                                @if ($un->file)
-                                                    <a target="blank" href="{{ $un->file }}" download><i class="fa fa-download"></i></a>
-                                                @endif
-                                                </td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                        <i class="fas fa-briefcase"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Usulan ST Pengembangan Profesi</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $stp }}
                         </div>
                     </div>
                 </div>
-            <div class="row">
             </div>
-        </section>
-    </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                        <i class="fas fa-road"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Usulan ST Perjalanan Dinas</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $stpd }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                        <i class="fas fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Usulan Surat Lain</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $sl }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Surat Saya</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped display responsive"
+                                id="table-pengelolaan-dokumen-pegawai">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nomor Surat</th>
+                                        <th>Tanggal Surat</th>
+                                        <th>Jenis Surat</th>
+                                        <th>Download</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($surat as $un)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{ $un->nomor_surat }}</td>
+                                        <td>{{ $un->tanggal }}</td>
+                                        <td>{{ $un->jenis }}</td>
+                                        <td>
+                                            @if ($un->file)
+                                            <a target="blank" href="{{ $un->file }}" download><i
+                                                    class="fa fa-download"></i></a>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+        </div>
+    </section>
+</div>
 @endsection
 
 @push('scripts')
-    <!-- JS Libraies -->
-    {{-- <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script> --}}
-    <script src="https://cdn.datatables.net/v/dt/dt-1.13.4/b-2.3.6/b-colvis-2.3.6/datatables.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/jszip/jszip.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/pdfmake/pdfmake.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/pdfmake/vfs_fonts.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-    <script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-    <script src="{{ asset('library') }}/sweetalert2/dist/sweetalert2.min.js"></script>
-    
-    <script src="{{ asset('js') }}/page/pegawai-pengelolaan-dokumen.js"></script>
+<!-- JS Libraies -->
+{{-- <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script> --}}
+<script src="https://cdn.datatables.net/v/dt/dt-1.13.4/b-2.3.6/b-colvis-2.3.6/datatables.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ asset('js') }}/plugins/jszip/jszip.min.js"></script>
+<script src="{{ asset('js') }}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{ asset('js') }}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{ asset('js') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="{{ asset('library') }}/sweetalert2/dist/sweetalert2.min.js"></script>
+
+<script src="{{ asset('js') }}/page/pegawai-pengelolaan-dokumen.js"></script>
 @endpush
