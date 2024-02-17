b9bacd3c4e616a469c50e12263b6b1c32a827b6b
nubisub <hendraksm18@mail.ugm.ac.id>
2024-02-18 00:58:41 +0700

    Refactor revisi_rencana_kinerja migration

--- database/migrations/2024_02_16_150204_revisi_rencana_kinerja.php
+++ database/migrations/2024_02_16_150204_revisi_rencana_kinerja.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('id_proyek')->nullable()->after('id_timkerja');
             $table->foreign('id_proyek')->references('id')->on('proyeks')->onDelete('cascade');
             // change id_hasilkerja tp foreign key to master_hasil_kerjas id
-            $table->dropColumn('id_hasilkerja');
-            $table->string('id_hasilkerja')->after('id_proyek');
+            // $table->dropColumn('id_hasilkerja');
+            // $table->string('id_hasilkerja')->after('id_proyek');
             $table->foreign('id_hasilkerja')->references('id')->on('master_hasil_kerjas')->onDelete('cascade');
             // remove mulai and selesai column
             $table->dropColumn('mulai');
