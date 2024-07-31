b7758f23ccc65ba7f665c69378a655fc3d87d566
nubisub <hendraksm18@mail.ugm.ac.id>
2024-07-31 19:51:55 +0700

    change environtment variable to config and force https schema in appserviceprovider

--- config/auth.php
+++ config/auth.php
@@ -18,6 +18,14 @@ return [
         'passwords' => 'users',
     ],
 
+    'sso' => [
+        'authServerUrl' => env('SSO_AUTH_SERVER_URL'),
+        'realm' => env('SSO_REALM'),
+        'clientId' => env('SSO_CLIENT_ID'),
+        'clientSecret' => env('SSO_CLIENT_SECRET'),
+        'redirectUri' => env('SSO_REDIRECT_URI'),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
@@ -94,6 +102,7 @@ return [
             'throttle' => 60,
         ],
     ],
+
 
     /*
     |--------------------------------------------------------------------------
